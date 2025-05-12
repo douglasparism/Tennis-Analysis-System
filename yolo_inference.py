36a5637eb4e01f51fb66a117224ce1e4d5ab5110
Raj Patel <raj721patel@gmail.com>
2025-05-12 17:28:08 -0500

    Adding dataset

--- yolo_inference.py
+++ yolo_inference.py
@@ -4,6 +4,9 @@ from ultralytics import YOLO
 model = YOLO('yolov8x')
 
 # to run model on an image within dir
-model.predict('input_video/image.png', save=True)
-
+result = model.predict('input_videos/input_video.mp4', save=True)
+print(result)
+print("Boxes:")
+for box in result[0].boxes:
+    print(box)
 
