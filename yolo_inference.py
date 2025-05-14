b14fd3330ed1480fa1e3863a13f563a32e1407e1
Raj Patel <raj721patel@gmail.com>
2025-05-13 20:23:20 -0500

    keypoint detection training

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
-# import a yolo model, 'x' stands for extra large
+# import a yolo model made from the models folder (will be empty since in repo since it is big)
 model = YOLO('yolov8x')
 
 # to run model on an image within dir
-result = model.predict('input_videos/input_video.mp4', save=True)
-print(result)
-print("Boxes:")
-for box in result[0].boxes:
-    print(box)
+result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
+# print(result)
+# print("Boxes:")
+# for box in result[0].boxes:
+#     print(box)
 
